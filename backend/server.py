daf2940a019664798be208ffceffc87294e07b5a
kghorbanzadeh23 <kamand.ghorbanzadeh@ucalgary.ca>
2024-03-25 12:51:43 -0600

    commit

--- backend/server.py
+++ backend/server.py
@@ -14,28 +14,29 @@ def get_products(product_id=None):
     products = load_products ()
     if product_id is None :
         # Return all products wrapped in an object with a ' products ' key
-        return jsonify ({" products ": products })
+        return jsonify({" products ": products })
     else:
-        product = next (( p for p in products if p [ ' id '] == product_id ),None )
-        # If a specific product is requested ,
-        # wrap it in an object with a ' products ' key
-        # Note : You might want to change this
-        # if you want to return a single product not wrapped in a list
-        return jsonify ( product ) if product else ( ' ' , 404)
+        product = next(( p for p in products if p ['id'] == product_id ),None )
+        if product:
+            # Return the specific product as a JSON response
+            return jsonify(product)
+        else:
+            # Return a 404 response if the specific product is not found
+            return {"message": "Product not found"}, 404
     
-@app . route ( '/products/add ' , methods =['POST'])
+@app.route ('/products/add', methods =['POST'])
 def add_product () :
-    new_product = request . json
-    products = load_products ()
-    new_product [ 'id'] = len ( products ) + 1
-    products.append ( new_product )
-    with open ( 'products.json ' , 'w ') as f :
+    new_product = request.json
+    products = load_products()
+    new_product ['id'] = len(products) + 1
+    products.append(new_product)
+    with open ( 'products.json' , 'w ') as f :
         json.dump ({"products": products } , f )
-    return jsonify( new_product ) , 201
+    return jsonify(new_product) , 201
 
-@app . route ( '/product-images/<path:filename> ')
-def get_image ( filename ) :
-    return send_from_directory ( 'product-images' , filename )
+@app.route ('/product-images/<path:filename>')
+def get_image(filename) :
+    return send_from_directory('product-images', filename )
 
 if __name__ == '__main__':
     app.run(debug=True)
